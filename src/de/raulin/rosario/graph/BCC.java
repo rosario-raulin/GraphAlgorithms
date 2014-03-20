aa67c6e36e54d0ed449483fd468a07efcbae3748
Rosario Raulin <Rosario@Rosarios-MacBook-Pro.local>
2014-03-21 00:01:04 +0100

    update BCC

--- src/de/raulin/rosario/graph/BCC.java
+++ src/de/raulin/rosario/graph/BCC.java
@@ -1,59 +1,68 @@
 package de.raulin.rosario.graph;
 
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by Rosario on 20.03.14.
  */
 public class BCC {
 
+    private Set<Integer> articulationPoints;
+    private int[] d;
     private int[] low;
+    private int count;
 
     public BCC(Graph graph) {
+        this.articulationPoints = new HashSet<Integer>();
+        this.d = new int[graph.V()];
         this.low = new int[graph.V()];
 
-        boolean[] marked = new boolean[graph.V()];
-        int[] d = new int[graph.V()];
+        for (int i = 0; i < graph.V(); ++i) {
+            d[i] = -1;
+        }
 
-        int time = 0;
         for (int v = 0; v < graph.V(); ++v) {
-            if (!marked[v]) {
-                time = visit(graph, time, marked, d, v, v);
+            if (d[v] == -1) {
+                visit(graph, v, v);
             }
         }
     }
 
-    public boolean connected(int v, int w) {
-        return low[v] == low[w];
-    }
-
-    private int visit(Graph graph, int time, boolean[] marked, int[] d, int v, int u) {
-        marked[v] = true;
-        d[v] = ++time;
+    private void visit(Graph graph, int v, int parent) {
+        d[v] = count++;
         low[v] = d[v];
+
+        int children = 0;
         for (Integer w : graph.adj(v)) {
-            if (!marked[w]) {
-                time = visit(graph, time, marked, d, w, v);
-                if (low[w] < low[v]) low[v] = low[w];
-            } else if (w != u) {
-                if (d[w] < low[v]) low[v] = d[w];
+            if (d[w] == -1) {
+                children++;
+                visit(graph, w, v);
+                if (low[w] < low[v]) {
+                    low[v] = low[w];
+                }
+                if (low[w] >= d[v] && parent != v) {
+                    articulationPoints.add(v);
+                }
+            } else if (w != parent) {
+                low[v] = Math.min(low[v], d[w]);
             }
         }
-        return ++time;
+
+        if (v == parent && children >= 2) {
+            articulationPoints.add(v);
+        }
+    }
+
+    public Iterable<Integer> articulationPoints() {
+        return articulationPoints;
     }
 
     public static void main(String[] args) {
         Graph graph = new Graph(new Scanner(System.in));
         BCC bcc = new BCC(graph);
 
-        System.out.println(graph.E());
-
-        for (int i = 0; i < graph.V(); ++i) {
-            for (int j = 0; j < graph.V(); ++j) {
-                if (bcc.connected(i, j)) {
-                    System.out.println(i + " und " + j + " liegen in der gleichen BCC");
-                }
-            }
+        for (Integer x : bcc.articulationPoints()) {
+            System.out.println(x);
         }
     }
 }
